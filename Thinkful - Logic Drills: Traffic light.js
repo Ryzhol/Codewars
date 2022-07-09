c6f931e0bd7ffdc643f7dc7bcc722b17e6a17835
Ryzhol <52762160+Ryzhol@users.noreply.github.com>
2022-07-09 06:22:59 +0300

    Create Thinkful - Logic Drills: Traffic light.js

---
+++ Thinkful - Logic Drills: Traffic light.js
@@ -0,0 +1,16 @@
+function updateLight(current) {
+  switch(current)
+    {
+        case "red":
+        current="green";
+        break;
+        case "yellow":
+        current="red"
+        break;
+        case "green":
+        current="yellow"
+        break;
+    }
+  return current;
+
+}
